289b262fa406fbc4d5f29cb660e436eb41af4669
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2015-01-30 17:58:27 +0000

    read instrument permissions

---
+++ env_trick.sh
@@ -0,0 +1,4 @@
+echo "export MYSQL_DATABASE=$MYSQL_DATABASE" >> /output/env.txt
+echo "export MYSQL_USER=$MYSQL_USER" >> /output/env.txt
+echo "export MYSQL_PASSWORD=$MYSQL_PASSWORD" >> /output/env.txt
+
--- python/read_users.py
+++ python/read_users.py
@@ -8,10 +8,34 @@ from sqlalchemy.sql import func
 from sqlalchemy.ext.automap import automap_base
 
 db_host = "mysql"
-db = os.getenv("MYSQL_DATABASE")
+db_name = os.getenv("MYSQL_DATABASE")
 db_user = os.getenv("MYSQL_USER")
 db_pass = os.getenv("MYSQL_PASSWORD")
 
-engine = create_engine('mysql://%s:%s@%s/%s') % (db_user,db_pass,db_host,db)
+conn_str = 'mysql+pymysql://%s:%s@%s/%s' % (db_user,db_pass,db_host,db_name)
+#print conn_str
+engine = create_engine(conn_str)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+session = Session(engine)
+
+account = Base.classes.accounts
+user = Base.classes.webcal_user
+resource = Base.classes.webcal_resource
+department = Base.classes.dept_lookup
+
+user_resource_x = Base.classes.user_resource_x
+user_accounts_x = Base.classes.user_accounts_x
+
+columns = [user.cal_login, \
+        resource.resource_name]
+queryAccess = session.query(*columns) \
+        .join(user_resource_x,user_resource_x.cal_login==user.cal_login) \
+        .join(resource,user_resource_x.resource_id==resource.resource_id)
+
+result = queryAccess.all()
+for r in result:
+    print r
 
 
